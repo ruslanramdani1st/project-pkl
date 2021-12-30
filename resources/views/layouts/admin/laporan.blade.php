a188a70a100082a89e2fe6d028efbf313d58cbcb
Agung Wahyudi <olipiskandar@gmal.com>
2021-12-30 12:59:43 +0700

    change

--- resources/views/layouts/admin/laporan.blade.php
+++ resources/views/layouts/admin/laporan.blade.php
@@ -30,18 +30,18 @@ ADMIN | UINSGD
                         @php $no = 1; @endphp
                         @foreach($pengguna as $data)
                         <tr scope="row">
-                            <td >
+                            <td>
                                 {{$no++}}
                             </td>
                             <td>
-                                {{-- {{$data->laporan->nip}} --}}
+                                {{$data->user->nip}}
                             </td>
                             <td>
-                                {{-- {{$data->laporan->name}} --}}
+                                {{$data->user->name}}
                             </td>
                             <td>
                                 <center>
-                                    {{-- {{$data->laporan->golongan}} --}}
+                                    {{$data->user->golongan}}
                                 </center>
                             </td>
                             <td>
